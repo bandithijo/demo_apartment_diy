623a754e5f35b52c4fb1f525767f16ef8a38e499
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-08-24 15:13:09 +0800

    Createa befora_action :configure_permitted_parameters for devise_parameter_sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_account
 
   private
 
   def set_account
     @account = Account.find_by(subdomain: request.subdomain)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :email, :password, :password_confirmation, account_attributes: [:subdomain]
+      ]
+    )
   end
 end
