ed65536d8323bb6875543356af96348dd531ad82
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-08-24 13:16:23 +0800

    Add callbacks before_action to set_account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_account
+
+  private
+
+  def set_account
+    @account = Account.find_by(subdomain: request.subdomain)
+  end
 end
